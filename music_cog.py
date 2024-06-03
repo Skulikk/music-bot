41ac4a6a4ae48d5d1d59c6803cbec005f59e643a
Skulikk <73563587+Skulikk@users.noreply.github.com>
2024-06-03 20:42:45 +0200

    Add files via upload

--- music_cog.py
+++ music_cog.py
@@ -2,7 +2,6 @@ from discord.ext import commands
 from discord.utils import get
 from youtubesearchpython import *
 from spotipy.oauth2 import SpotifyClientCredentials
-from pprint import pprint
 
 import os
 import discord
@@ -27,6 +26,7 @@ class music(commands.Cog):
         self.predownloading = False
         self.update_req = 0
         self.on_repeat = False
+        self.change = None
 
         self.queue_message = None
 
@@ -45,18 +45,15 @@ class music(commands.Cog):
     async def shuffle_queue(self):
         random.shuffle(self.song_queue)
         await self.send_queue_embed()
-        predownloaded_loc = getattr(self, "predownloaded")
-        print(predownloaded_loc)
+
+    async def error_message(self, message):
+        msg = await self.ctx.send(message)
+        await asyncio.sleep(3)
+        msg.delete()
 
     def clear_queue(self):
         self.song_queue.clear()
         self.queue_message = None
-
-    def repeat(self):
-        if(self.on_repeat):
-            self.on_repeat = False
-        else:
-            self.on_repeat = True
 
     def YT_search(self, query, direct):
         #explicit url mode
@@ -64,14 +61,12 @@ class music(commands.Cog):
             res = Video.getInfo(query)
             #no result
             if (not res):
-                #await self.error_message("Skladba " + query + " nebyla nalezena")
                 return 0
             res = res["link"], res["title"], int(res["duration"]["secondsText"]), res["thumbnails"][0]["url"]
         else:
             videosSearch = VideosSearch(query, limit = 1)
             #no result
             if (not videosSearch.result()["result"]):
-                #await self.error_message("Skladba " + query + " nebyla nalezena")
                 return 0
             res = videosSearch.result()["result"][0]
 
@@ -137,7 +132,7 @@ class music(commands.Cog):
                 with yt_dlp.YoutubeDL(ydl_opts) as ydl:
                     ydl.download(url[0])
             except:
-                #something is fucked
+                #something fucked up
                 return 1
         return 0
     
@@ -147,16 +142,16 @@ class music(commands.Cog):
             self.embed_queue.clear_fields()
         except:
             pass
-        self.embed_queue = discord.Embed(title="Fronta", color=0x0000ff)
+        self.embed_queue = discord.Embed(title="Queue", color=0x0000ff)
 
         if (len(self.song_queue) > 24):
             if (len(self.queue_for_queue) > 0):
-                self.embed_queue.set_footer(text="Skladeb ve frontě: " + str(len(self.song_queue)) + "\nFronta zobrazuje pouze prvních 24 skladeb.\nProbíhá přidávání skladeb do fronty. Zbývá přidat: " + str(len(self.queue_for_queue)))
+                self.embed_queue.set_footer(text="Queued tracks: " + str(len(self.song_queue)) + "\nOnly first 24 tracks are shown.\nAdding to the queue in progress. Tracks left to add: " + str(len(self.queue_for_queue)))
             else:    
-                self.embed_queue.set_footer(text="Skladeb ve frontě: " + str(len(self.song_queue)) + "\nFronta zobrazuje pouze prvních 24 skladeb.")
+                self.embed_queue.set_footer(text="Queued tracks: " + str(len(self.song_queue)) + "\nOnly first 24 tracks are shown.")
         else:
             if (len(self.queue_for_queue) > 0):
-                self.embed_queue.set_footer(text="Skladeb ve frontě: " + str(len(self.song_queue)) + "\nProbíhá přidávání skladeb do fronty. Zbývá přidat: " + str(len(self.queue_for_queue)))
+                self.embed_queue.set_footer(text="Queued tracks: " + str(len(self.song_queue)) + "\nAdding to the queue in progress. Tracks left to add: " + str(len(self.queue_for_queue)))
         
         if (len(self.song_queue)) >= 1:
             for song in self.song_queue:
@@ -165,13 +160,13 @@ class music(commands.Cog):
                 self.embed_queue.add_field(name=str(i)+"   "+song[1], value="", inline=False)
                 i += 1
         else:
-            self.embed_queue.add_field(name="Fronta je prázdná, další skladbu přidej příkazem !play ...", value="", inline=False)
+            self.embed_queue.add_field(name="The queue is empty. You can change it with \"!play ...\" command", value="", inline=False)
 
         #queue msg already exists
         if (self.queue_message):
             await self.queue_message.edit(embed=self.embed_queue)
         else:
-            button_view=View_queue_buttons(self.bot, self.ctx.guild, self.song_queue, self.ctx, None, self.predownloaded, timeout=None)
+            button_view=View_queue_buttons(self, self.bot, self.ctx.guild, self.song_queue, self.ctx, None, self.predownloaded, timeout=None)
             self.queue_message = await self.ctx.send(embed=self.embed_queue, view=button_view)
             button_view.queue_message = self.queue_message
 
@@ -191,27 +186,37 @@ class music(commands.Cog):
         embed_player = discord.Embed(title="***" + name + "***", color=0x0000ff)
         embed_player.set_thumbnail(url=thumbnail)
         embed_player.add_field(name=line + " " + sec_min(duration) + " s", value="** **", inline=True)
+        if(len(self.song_queue)):
+            embed_player.set_footer(text="Next up: " + self.song_queue[0][1])
         await message.edit(embed=embed_player)
 
         skip = duration/200
 
         #wait here till the song finishes
         while(not self.task_done):
-            if(self.predownloaded and not predownload_sent):
-                embed_player.set_footer(text="Next: " + self.song_queue[0][1] + " (preloaded)")
-                await message.edit(embed=embed_player)
-                predownload_sent = 1
 
-            print(self.predownloaded)
-            print(predownload_sent)
+            if(not self.on_repeat):
+                if(self.predownloaded and not predownload_sent):
+                    embed_player.remove_footer()
+                    if(len(self.song_queue)):
+                        embed_player.set_footer(text="Next up: " + self.song_queue[0][1] + " (preloaded)")
+                    await message.edit(embed=embed_player)
+                    predownload_sent = 1
 
-            if(not self.predownloaded and predownload_sent):
+
+                #queue shuffled
+                if(not self.predownloaded and predownload_sent):
+                    embed_player.remove_footer()
+                    if(len(self.song_queue)):
+                        embed_player.set_footer(text="Next up: " + self.song_queue[0][1])
+                    await message.edit(embed=embed_player)
+                    predownload_sent = 0
+                    embed_player.remove_footer
+            else:
                 embed_player.remove_footer()
-                embed_player.set_footer(text="Next: " + self.song_queue[0][1])
-                await message.edit(embed=embed_player)
-                predownload_sent = 1
-                embed_player.remove_footer
-        
+                embed_player.set_footer(text="Next up: " + name)
+                predownload_sent = 0
+
             if (self.update_req > 5 or (self.update_req and not len(self.queue_for_queue))):
                 await self.send_queue_embed()
                 self.update_req = 0
@@ -250,10 +255,10 @@ class music(commands.Cog):
     async def play(self, ctx:commands.Context, *args):
 
         #wrong text channel error msg
-        #if (ctx.channel.id != self.dj_channel):
-        #    channel_name = self.bot.get_channel(int(self.dj_channel))
-        #    await ctx.send("<:point_right:1013917029859868682> #" + str(channel_name))
-        #    return
+        if (ctx.channel.id != self.dj_channel):
+            channel_name = self.bot.get_channel(int(self.dj_channel))
+            await ctx.send("<:point_right:1013917029859868682> #" + str(channel_name))
+            return
         
         self.ctx = ctx
 
@@ -279,7 +284,7 @@ class music(commands.Cog):
             if len(args) == 2:
                 length = int(args[1])
                 if (int(args[1]) > 20 or int(args[1]) < 2):
-                    await self.error_message("***Špatná hodnota přepínače -q.***")
+                    await self.error_message("***Wrong use of -q***")
                     return
             else:
                 length = 10
@@ -294,9 +299,9 @@ class music(commands.Cog):
                     line = [line.split("$")[0]]
                     music_list.append(line)
 
-            #if line_count < length:
-            #    #await self.error_message("***V databázi není dostatek záznamů.***")
-            #    return
+            if line_count < length:
+                await self.error_message("***not enought database entries***")
+                return
             
             #random numbers for list shuffle
             song_numbers = random.sample(range(0, (len(music_list) - 1)), length)
@@ -325,31 +330,38 @@ class music(commands.Cog):
                     search = search
                 URI = search.split("/")[-1].split("?")[0]
 
-                #try:
-                if re.match(r"^https:\/\/open.spotify.com\/playlist\/[a-zA-Z0-9]*", search):
-                    track = self.sp.playlist_tracks(URI)["items"][0]
-                    artist_uri = track["track"]["artists"][0]["uri"]
-                    self.queue_for_queue.append(track["track"]["name"] + " " + self.sp.artist(artist_uri)["name"])
-                    for i in range (0, 50):
-                        fetch = threading.Thread(target=self.SPOT_fetch, args=(URI, i, 1))
-                        fetch.start()
-                else:
-                    track = self.sp.album_tracks(URI)["items"][0]
-                    self.queue_for_queue.append(track["name"] + " " + track["artists"][0]["name"])
-                    for i in range (0, 50):
-                        fetch = threading.Thread(target=self.SPOT_fetch, args=(URI, i, 2))
-                        fetch.start()
+                try:
+                    if re.match(r"^https:\/\/open.spotify.com\/playlist\/[a-zA-Z0-9]*", search):
+                        track = self.sp.playlist_tracks(URI)["items"][0]
+                        artist_uri = track["track"]["artists"][0]["uri"]
+                        self.queue_for_queue.append(track["track"]["name"] + " " + self.sp.artist(artist_uri)["name"])
+                        for i in range (0, 50):
+                            fetch = threading.Thread(target=self.SPOT_fetch, args=(URI, i, 1))
+                            fetch.start()
+                    else:
+                        track = self.sp.album_tracks(URI)["items"][0]
+                        self.queue_for_queue.append(track["name"] + " " + track["artists"][0]["name"])
+                        for i in range (0, 50):
+                            fetch = threading.Thread(target=self.SPOT_fetch, args=(URI, i, 2))
+                            fetch.start()
 
-                #except Exception as e:
-                #    print(e)
-                #    return
+                except Exception as e:
+                    print(e)
+                    return
             else:
                 self.queue_for_queue.append(search)
+
+        file = open(self.database_file,'r+')
 
         #first track, search for details directly
         if not(voice and voice.is_connected()):
             url = self.YT_search(self.queue_for_queue.pop(), 1)
-            print(url)
+            if url[0] not in file.read():
+                try:
+                    file.write(url[0]+"$"+url[1]+"\n")
+                except:
+                    file.write(url[0]+"$"+"encoding err\n")
+
             voiceChannel = ctx.author.voice.channel
             voice = await voiceChannel.connect()
 
@@ -357,7 +369,7 @@ class music(commands.Cog):
             embed_player.add_field(name="Loading...", value="** **", inline=True)
             embed_player.set_thumbnail(url=url[3])
             await self.send_queue_embed()
-            message = await ctx.send(embed=embed_player, view=View_buttons(self.bot.voice_clients, ctx.guild, self.song_queue, ctx, self.queue_message, timeout=None))
+            message = await ctx.send(embed=embed_player, view=View_buttons(self, self.bot.voice_clients, ctx.guild, self.song_queue, ctx, self.queue_message, timeout=None))
 
             try:
                 os.remove('downloaded.m4a')
@@ -370,41 +382,47 @@ class music(commands.Cog):
 
             while (len(self.song_queue) >= 1):
                 self.task_done = False
-                try:
-                    os.remove('downloaded.m4a')
-                except:
-                    pass
+                if(not self.on_repeat):
+                    try:
+                        os.remove('downloaded.m4a')
+                    except:
+                        pass
 
-                url = self.song_queue.pop(0)
+                    url = self.song_queue.pop(0)
+
+                    if url[0] not in file.read():
+                        try:
+                            file.write(url[0]+"$"+url[1]+"\n")
+                        except:
+                            file.write(url[0]+"$"+"encoding err\n")
+
+                    if(self.predownloaded):
+                        try:
+                            os.rename('predownloaded.m4a', 'downloaded.m4a')
+                        except:
+                            pass
+                        self.predownloaded = False 
+                    else:
+                        self.YT_download(url)
 
                 line = "⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛"
                 embed_player = discord.Embed(title="***" + url[1] + "***", color=0x0000ff)
                 embed_player.add_field(name="Loading...", value="** **", inline=True)
                 embed_player.set_thumbnail(url=url[3])
-                
                 await message.edit(embed=embed_player)
                 await self.send_queue_embed()
-
-                if(self.predownloaded):
-                    try:
-                        os.rename('predownloaded.m4a', 'downloaded.m4a')
-                    except:
-                        pass
-                    self.predownloaded = False 
-                else:
-                    self.YT_download(url)
 
                 await self.play_task(voice, url[2], message, url[1], url[3])
 
 class View_buttons(discord.ui.View, music):
-    def __init__(self, voice_clients, voice_guilt, song_queue, ctx, queue_message, *args, **kwargs):
+    def __init__(self, parent, voice_clients, voice_guilt, song_queue, ctx, queue_message, *args, **kwargs):
         super(View_buttons, self).__init__(*args, **kwargs)
         self.voice = discord.utils.get(voice_clients, guild=voice_guilt)
         self.song_queue = song_queue
         self.ctx = ctx
         self.queue_message = queue_message
         self.queue_for_queue = []
-        self.on_repeat = False
+        self.parent = parent
 
     # Na začátku to hraje, tak tam je hned Pause
     @discord.ui.button(label="Pause", style=discord.ButtonStyle.green, emoji="⏸️")
@@ -434,25 +452,23 @@ class View_buttons(discord.ui.View, music):
     @discord.ui.button(label="Next", style=discord.ButtonStyle.blurple, emoji="⏭️")
     async def next_callback(self, interaction: discord.Interaction, button: discord.ui.button):
         if (not self.song_queue):
-           await interaction.response.send_message(content="Fronta je prázdná", ephemeral=True, delete_after = 5)
+           await interaction.response.send_message(content="Queue is empty", ephemeral=True, delete_after = 5)
            return
         self.voice.stop()
         await interaction.response.defer()
     @discord.ui.button(label="Repeat", style=discord.ButtonStyle.grey, emoji="🔂")
     async def repeat_callback(self, interaction: discord.Interaction, button: discord.ui.button):
-        #if (not self.song_queue):
-        #   await interaction.response.send_message(content="Fronta je prázdná", ephemeral=True, delete_after = 5)
-        #   return
-        self.repeat()
-        if (self.on_repeat):
+        if (not self.parent.on_repeat):
             button.style = discord.ButtonStyle.success
+            self.parent.on_repeat = True
         else:
             button.style = discord.ButtonStyle.grey
+            self.parent.on_repeat = False
         
         await interaction.response.edit_message(view=self)
 
 class View_queue_buttons(discord.ui.View, music):
-    def __init__(self, bot, voice_guilt, song_queue, ctx, queue_message, predownloaded, *args, **kwargs):
+    def __init__(self, parent, bot, voice_guilt, song_queue, ctx, queue_message, predownloaded, *args, **kwargs):
         super(View_queue_buttons, self).__init__(*args, **kwargs)
         self.bot = bot
         self.voice = discord.utils.get(bot.voice_clients, guild=voice_guilt)
@@ -460,15 +476,15 @@ class View_queue_buttons(discord.ui.View, music):
         self.ctx = ctx
         self.queue_message = queue_message
         self.queue_for_queue = []
-        self.predownloaded = False
+        self.parent = parent
 
     @discord.ui.button(label="Shuffle", style=discord.ButtonStyle.grey, emoji="🔀")
     async def shuffle_callback(self, interaction: discord.Interaction, button: discord.ui.button):
         if (not self.song_queue):
-           await interaction.response.send_message(content="Fronta je prázdná", ephemeral=True, delete_after = 5)
+           await interaction.response.send_message(content="Queue is empty", ephemeral=True, delete_after = 5)
            return
         await music.shuffle_queue(self)
-        print("shuff")
+        self.parent.predownloaded = False
         await interaction.response.defer()
 
 #converts seconds to MM:SS format
